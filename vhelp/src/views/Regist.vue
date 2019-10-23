80e0bffb55a3e55662b9aae9c49e512f373d60de
jl <904880305@qq.com>
2019-10-23 17:48:55 +0800

    change regist

--- vhelp/src/views/Regist.vue
+++ vhelp/src/views/Regist.vue
@@ -1,10 +1,13 @@
 <template>
 	<div id="regist">
-		<van-row>
-			<van-col span="8">span: 8</van-col>
-			<van-col span="8">span: 8</van-col>
-			<van-col span="8">span: 8</van-col>
+		<van-row class="registcon">
+			<van-col span="24">
+				<van-image :src="logo" />
+			</van-col>		
 		</van-row>
+		<van-cell-group>
+            <van-field v-model="username" placeholder="请输入用户名" />
+		</van-cell-group>
 	</div>
 </template>
 
@@ -12,17 +15,29 @@
 
 import Vue from 'vue';
 import { Row, Col } from 'vant';
+import { Image } from 'vant';
+import { Cell, CellGroup } from 'vant';
+import { Field } from 'vant';
 
 Vue.use(Row).use(Col);
+Vue.use(Image);
+Vue.use(Field);
+Vue.use(Cell).use(CellGroup);
 
 export default {
 	name:'regist',
+	data:function(){
+		return {
+			logo:require('../assets/images/logo.png'),
+			username:''
+		}
+	},
 	components:{
-		
-	}
 
+	}
 }
 </script>
 
 <style>
+	.registcon{margin-top:15%;}
 </style>
