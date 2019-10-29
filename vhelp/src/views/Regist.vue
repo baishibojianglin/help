209d95b49587abeff40fc36ea211492ab3972d72
jlnotebook <904880305@qq.com>
2019-10-29 23:51:00 +0800

    login

--- vhelp/src/views/Regist.vue
+++ vhelp/src/views/Regist.vue
@@ -1,14 +1,31 @@
 <template>
 	<div id="regist">
-		<van-row class="registcon">
+<!-- 		<van-row class="registcon">
 			<van-col span="24">
-				<van-image :src="logo" />
+				<van-image round width='10rem' :src="logo" />
 			</van-col>		
-		</van-row>
-		<van-cell-group>
-            <van-field v-model="username" placeholder="请输入用户名" />
-			<van-field type="password" v-model="password" placeholder="请输入用户名" />
-		</van-cell-group>
+		</van-row> -->
+		
+		<van-row class="registcon">
+			<van-col span="24">手机号码</van-col>
+			<van-col span="24">
+				<input type="number" />
+			</van-col>		
+		</van-row>		
+		<van-password-input
+			:value="value"
+			:focused="showKeyboard"
+			@focus="showKeyboard = true"
+		/>
+
+		<!-- 数字键盘 -->
+		<van-number-keyboard
+			:show="showKeyboard"
+			@input="onInput"
+			@delete="onDelete"
+			@blur="showKeyboard = false"
+		/>	
+		
 	</div>
 </template>
 
@@ -17,29 +34,52 @@
 import Vue from 'vue';
 import { Row, Col } from 'vant';
 import { Image } from 'vant';
-import { Cell, CellGroup } from 'vant';
-import { Field } from 'vant';
+import { PasswordInput, NumberKeyboard } from 'vant';
 
 Vue.use(Row).use(Col);
 Vue.use(Image);
-Vue.use(Field);
-Vue.use(Cell).use(CellGroup);
+Vue.use(PasswordInput).use(NumberKeyboard);
 
 export default {
 	name:'regist',
 	data:function(){
 		return {
-			logo:require('../assets/images/logo.png'),
+			//logo:require('../assets/images/logo.png'),
 			username:'',
-			password:''
+			password:'',
+            value: '',
+            showKeyboard:false
 		}
 	},
 	components:{
 
+	},
+	methods: {
+			onInput(key) {
+			this.value = (this.value + key).slice(0, 6);
+			},
+			onDelete() {
+			this.value = this.value.slice(0, this.value.length - 1);
+			}
 	}
 }
 </script>
 
 <style>
-	.registcon{margin-top:15%;}
+	
+	#regist{
+		position: fixed;
+		left: 0;
+		right: 0;
+		top:0;
+		bottom: 0;
+		background-image: url(../assets/images/login.jpg);
+		background-size: 100% 100%;
+	}
+	.registcon{
+		margin-top:45%;
+	}
+	.telcon{
+		
+	}
 </style>
