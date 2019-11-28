25c64809e70b764575677881fb56e2b7c4ce58e4
jl <904880305@qq.com>
2019-11-28 15:48:23 +0800

    aes

--- uhelp/common/js/aes.js
+++ uhelp/common/js/aes.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-import CryptoJS from '@/node_modules/crypto-js/crypto-js.js';
-
-
-var key = CryptoJS.enc.Utf8.parse("1234123412341234");
-
-//解密方法
-function Decrypt(word) {
-  var decrypt = CryptoJS.AES.decrypt(word, key, {
-    mode: CryptoJS.mode.ECB,
-    padding: CryptoJS.pad.Pkcs7
-  });
-  return CryptoJS.enc.Utf8.stringify(decrypt).toString();
-}
-=======
 import CryptoJS from '@/node_modules/crypto-js/crypto-js.js';
 var key = CryptoJS.enc.Utf8.parse("1234123412341234");
 var iv = "1234567812345678";
->>>>>>> 2360f3bc14239c37893a506d7d76379c3569c55c
+
 
 //加密方法
 function Encrypt(word) {
