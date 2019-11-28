24a6feaded2e47a8a1b42bd14d364485742bc6df
jl <904880305@qq.com>
2019-11-28 11:28:41 +0800

    merge

--- adminhelp/application/matchmaker/controller/Login.php
+++ adminhelp/application/matchmaker/controller/Login.php
@@ -1,10 +1,7 @@
 <?php
 namespace app\matchmaker\controller;
 use app\common\lib\Aes;
-<<<<<<< HEAD
 
-=======
->>>>>>> 2360f3bc14239c37893a506d7d76379c3569c55c
 
 
 
@@ -12,14 +9,7 @@ class Login extends Common
 {
     public function login()
     {
-<<<<<<< HEAD
-       //$str=input('post.text');
-       $str='QYY+uYR3xIA4Mu5SP6Ni9Q==';
-       $key='1234123412341234';
-       $aes=new Aes();
-       $data=$aes->decrypt($str,$key);
-       return json($data);
-=======
+
           // $str=input();
           // $key='1234123412341234';
           // $aes=new Aes();
@@ -27,7 +17,7 @@ class Login extends Common
           // //parse_str($data,$arry);
           // return json($data); 
          //return json($str);    
->>>>>>> 2360f3bc14239c37893a506d7d76379c3569c55c
+
     }
 }
 
