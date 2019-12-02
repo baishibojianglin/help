f59e251fdea9357d28ccd1f4149fb0a162fa9a4d
jl <904880305@qq.com>
2019-12-02 17:46:18 +0800

    jl

--- adminhelp/application/matchmaker/controller/Login.php
+++ adminhelp/application/matchmaker/controller/Login.php
@@ -6,7 +6,8 @@ use app\common\lib\Aes;
 class Login extends Common
 {
     /**
-     * 登录
+     * 村红娘登录
+     *@return array message
      */
     public function login()
     {   
@@ -16,20 +17,42 @@ class Login extends Common
            $str=$aes->decrypt($str['text']);
            //字符串分解成变量
            parse_str($str,$data);
+           
+           //检测登录信息是否正确
+           if( empty($data['phone'])){
+             $message['status']=0;
+             $message['word']='请填写手机号';
+             return json($message); 
+           }
+           if( empty($data['password'])){
+             $message['status']=0;
+             $message['word']='请填写登录密码';
+             return json($message); 
+           }
+
            //密码md5加密
            $data['password']=md5($data['password']).'dlst';
+           
+           //查询数据表模型类
+           $list=model('Matchuser')->checklogin($data['phone'],$data['password']);
+           
+           //验证手机号或密码是否匹配
+           if(empty($list)){
+             $message['status']=0;
+             $message['word']='手机号或密码错误';
+             return json($message);
+           } 
 
-
+           //信息正确,放行登录 
+           $message['status']=1;
+           $message['word']='登录成功';
+           $message['data']=$list;
+           
+           return json($message);          
 
            
-             $matchuser=new Matchuser();
-             $list=$matchuser->checklogin($data['phone'],$data['password']);
-             return json($list);
-
-
-
-             
     }
+
 
 }
 
--- adminhelp/application/matchmaker/model/Matchuser.php
+++ adminhelp/application/matchmaker/model/Matchuser.php
@@ -2,8 +2,19 @@
 namespace app\matchmaker\model;
 use think\Model;
 
+/**
+ * 村红娘数据表模型类
+ */
 class Matchuser extends  Model
 {
-
+    /**
+	 * 登录
+	 */ 
+	 public function checklogin($phone,$password){
+         $map['phone']=$phone;
+         $map['password']=$password;
+         $list=$this->where($map)->find();
+         return $list;
+	 }   
 }
 
